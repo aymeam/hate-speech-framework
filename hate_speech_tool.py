38c39e53c16d1c58f8737d8d0eed4f4c89c636e0
Ayme <arangoayme@gmail.com>
2021-08-23 12:03:24 -0400

    add example of use

--- hate_speech_tool.py
+++ hate_speech_tool.py
@@ -2,25 +2,29 @@ from utils import *
 CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 print(CURR_DIR)
 sys.path.append(CURR_DIR + "/ACL19")
-# sys.path.append(CURR_DIR + "/ECML PKDD 2020")
+sys.path.append(CURR_DIR + "/ECML PKDD 2020")
+sys.path.append(CURR_DIR + "/WEBSCI21")
 sys.path.append(CURR_DIR + "/EMNLP20")
 
 from ACL19 import *
-# from ECMLPKDD20 import *
+from ECMLPKDD20 import *
 from EMNLP20 import *
 
 class Models():
     def __init__(self, setup):
       super(Models, self).__init__()
       self.ACL19 = ACL19(setup)
-      # self.ECMLPKDD20 = ECMLPKDD20(setup)
+      self.ECMLPKDD20 = ECMLPKDD20(setup)
+      self.WEBSCI21 = WEBSCI21(setup)
       self.EMNLP20 = EMNLP20(setup)
 
     def load_model(self,model_name):
       if model_name == "ACL":  
         return self.ACL19
-      # if model_name == "ECMLPKDD20":  
-      #   return self.ECMLPKDD20
+      if model_name == "ECMLPKDD20":  
+        return self.ECMLPKDD20
+      if model_name == "WEBSCI21":  
+        return self.WEBSCI21
       if model_name == "EMNLP20":  
         return self.EMNLP20
 
